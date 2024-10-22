ff7bc56122b90da7c88cc04acb52a901d9d48332
MishC <michaela.chovancova@gmail.com>
2024-10-22 14:59:05 +0200

    timestamp added

--- Classes/TodoItem.cs
+++ Classes/TodoItem.cs
@@ -4,9 +4,10 @@
     public int Id { get; private set; }
     public string Title { get; set; } = string.Empty;
     public bool Completed { get; set; } = false;
-
+    public DateTime CurrentDate { get; private set; } = DateTime.Now; 
     public TodoItem()
     {
         Id = _nextId++;
+        CurrentDate = DateTime.Now;
     }
 }
