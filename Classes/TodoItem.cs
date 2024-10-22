a3ba86638c9a299f49b81f5384f0ea7e4aefb1ab
MishC <michaela.chovancova@gmail.com>
2024-10-22 15:54:15 +0200

    timecompletion added, time difference added

--- Classes/TodoItem.cs
+++ Classes/TodoItem.cs
@@ -4,10 +4,20 @@
     public int Id { get; private set; }
     public string Title { get; set; } = string.Empty;
     public bool Completed { get; set; } = false;
-    public DateTime CurrentDate { get; private set; } = DateTime.Now; 
+    public DateTime CurrentDate { get; private set; } = DateTime.Now;
+    public DateTime TimeCompleted { get; set; }
+
     public TodoItem()
     {
         Id = _nextId++;
         CurrentDate = DateTime.Now;
+    }
+    public string TimeDifference
+    {
+        get
+        {
+            TimeSpan timeSpan = CurrentDate - TimeCompleted;
+            return $"{timeSpan.Days} days and {timeSpan.Hours} hours";
+        }
     }
 }
