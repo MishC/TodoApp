f21dddeaa11872d737fad62f74b5e6864c974f63
MishC <michaela.chovancova@gmail.com>
2024-10-22 13:21:05 +0200

    UI design with bootstrap and emoji

---
+++ Classes/EmojiService.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Text;
+
+public class EmojiService
+{
+    public int HighSurrogate;
+    public int? LowSurrogate;
+
+    public byte[] GetEmojiBytes()
+    {
+        char[] emojiChars;
+
+        if (LowSurrogate.HasValue)
+        {
+            emojiChars = new char[] { (char)HighSurrogate, (char)LowSurrogate.Value };
+        }
+        else
+        {
+            emojiChars = new char[] { (char)HighSurrogate };
+        }
+
+        byte[] bytes = Encoding.Unicode.GetBytes(emojiChars);
+
+        return bytes;
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,3 @@
+function showCustomAlert(message) {
+    alert(message);
+}
